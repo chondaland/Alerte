6cd2ff77fc6276aac4c7886a140f6b638bac1628
forum2014 <barthelemybidot@gmail.com>
2014-02-03 14:44:30 +0100

    Modif photo en plante

---
+++ src/Plante.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.ArrayList;
+
+public class Plante {
+	
+		private ArrayList plante = new ArrayList<Pixel>();
+		private int k;
+		private double epsilon;
+		
+		public ArrayList getA(){
+			return this.plante;
+		}
+		
+		
+		public Plante(ArrayList<Pixel> plante){
+			this.plante=plante;
+		}
+		
+		public void kmoyenne(){
+			
+		}
+		
+		public void kPPV(){
+			
+		private ArrayList 
+		}
+
+	}
