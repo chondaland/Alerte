3c7b291630606b8189d6e3bab1130f0e55fae4ef
Agathe Gilain <agathe.gilain@telecom-paristech.fr>
2014-02-03 14:09:20 +0100

    Beginning of the Code for k moyenne

---
+++ src/Photo.java
@@ -0,0 +1,15 @@
+
+public class Photo {
+	private ArrayList<Vector(4)>() a; 
+	
+	public ArrayList<Vector(4)>() getA(){
+		return this.a;
+	}
+	
+	public Photo(ArrayList<Vector(4)> a){
+		this.a=a;
+	}
+	
+	
+
+}
---
+++ src/Pixel.java
@@ -0,0 +1,39 @@
+
+public class Pixel {
+	private double R;
+	private double B;
+	private double NDVI;
+	private double IR;
+	private int numgroupe;
+	
+	public Pixel(double R, double B, double NDVI, double IR){
+		this.R=R;
+		this.B=B;
+		this.NDVI=NDVI;
+		this.IR=IR;
+	}
+	
+	public void setGroupe(int newnum){
+		this.numgroupe=newnum;
+	}
+	
+	public int getnumGroupe(){
+		return this.numgroupe;
+	}
+	
+	public double getR(){
+		return this.R;
+	}
+	
+	public double getB(){
+		return this.B;
+	}
+	
+	public double getNDVI(){
+		return this.NDVI;
+	}
+	
+	public double getIR(){
+		return this.IR;
+	}
+}
